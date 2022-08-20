c6154542b878daec189a4e7037a7ccf772492fc9
ethanejohnsons <ethan@ejohnsons.com>
2022-08-20 11:28:16 -0500

    Fixed spyglass gui texture

--- src/main/java/dev/lazurite/polaroid/client/mixin/render/GuiMixin.java
+++ src/main/java/dev/lazurite/polaroid/client/mixin/render/GuiMixin.java
@@ -1,23 +1,30 @@
 package dev.lazurite.polaroid.client.mixin.render;
 
-import com.mojang.blaze3d.systems.RenderSystem;
+import dev.lazurite.polaroid.Polaroid;
 import dev.lazurite.polaroid.client.PolaroidClient;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(Gui.class)
 public class GuiMixin {
-    @Redirect(
+    @Shadow @Final private Minecraft minecraft;
+
+    @ModifyArgs(
             method = "renderSpyglassOverlay",
             at = @At(
                     value = "INVOKE",
                     target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/resources/ResourceLocation;)V"
             )
     )
-    private void setShaderTexture(int id, ResourceLocation resourceLocation) {
-        RenderSystem.setShaderTexture(0, PolaroidClient.CAMERA_SCOPE);
+    private void setShaderTexture(Args args) {
+        if (this.minecraft.player != null && this.minecraft.player.getUseItem().is(Polaroid.CAMERA_ITEM)) {
+            args.set(1, PolaroidClient.CAMERA_SCOPE);
+        }
     }
 }
