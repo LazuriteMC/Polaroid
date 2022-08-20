4a5722d6c479477a288dc12e843e4c137c5ea661
ethanejohnsons <ethan@ejohnsons.com>
2022-08-20 11:31:38 -0500

    Version bump

--- src/main/java/dev/lazurite/polaroid/client/mixin/render/GuiMixin.java
+++ src/main/java/dev/lazurite/polaroid/client/mixin/render/GuiMixin.java
@@ -2,8 +2,10 @@ package dev.lazurite.polaroid.client.mixin.render;
 
 import dev.lazurite.polaroid.Polaroid;
 import dev.lazurite.polaroid.client.PolaroidClient;
+import dev.lazurite.polaroid.item.CameraItem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.world.item.SpyglassItem;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,6 +13,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
+/**
+ * Swaps out the "scoped-in" texture used by the {@link SpyglassItem} for the one used by the {@link CameraItem}.
+ */
 @Mixin(Gui.class)
 public class GuiMixin {
     @Shadow @Final private Minecraft minecraft;
